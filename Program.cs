48356f8476592c4b8d3fa1b3995411e1f456be2e
Best By <Best By@DESKTOP-Q9612MS>
2024-09-24 20:27:36 +0300

    CreateDev

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace MVC
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Hello World");
 
             app.Run();
         }
